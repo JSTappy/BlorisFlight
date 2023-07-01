f9e9e2ee29cd0089bdc67b47b1e1bbdbec7e6e88
Joas Sahetapy <jse.sahetapy@student.alfa-college.nl>
2023-07-01 15:20:39 +0200

    Player getters and better bullets

--- Bloris/Player.cpp
+++ Bloris/Player.cpp
@@ -2,14 +2,17 @@
 
 Player::Player()
 {
-	fireRate = 0;
+	bulletoffset = glm::vec3(0, 40.0f, 0);
+	dead = false;
+	_fireRate = 0;
 	shootdelay = new Timer();
 	beamdelay = new Timer();
-	speed = 200.0f;
+	_speed = 200.0f;
 	health = 3;
-	level = 1;
-	bulletspeed = 400;
-	bulletdamage = 1;
+	maxHealth = 3;
+	_level = 1;
+	_bulletspeed = 400;
+	_bulletdamage = 1;
 	this->AddChild(beamdelay);
 	this->AddChild(shootdelay);
 	beamdelay->StopTimer();
@@ -18,7 +21,8 @@ Player::Player()
 
 Player::~Player()
 {
-
+	shootdelay = nullptr;
+	beamdelay = nullptr;
 }
 
 void Player::update(float deltaTime)
@@ -26,62 +30,64 @@ void Player::update(float deltaTime)
 	//Movement
 	if (input()->GetKey(KEY_W))
 	{
-		this->position -= glm::vec3(0.0f, speed, 0.0f) * deltaTime;
+		this->position -= glm::vec3(0.0f, _speed, 0.0f) * deltaTime;
 	}
 	if (input()->GetKey(KEY_A))
 	{
-		this->position -= glm::vec3(speed, 0.0f, 0.0f) * deltaTime;
+		this->position -= glm::vec3(_speed, 0.0f, 0.0f) * deltaTime;
 	}
 	if (input()->GetKey(KEY_S))
 	{
-		this->position += glm::vec3(0.0f, speed, 0.0f) * deltaTime;
+		this->position += glm::vec3(0.0f, _speed, 0.0f) * deltaTime;
 	}
 	if (input()->GetKey(KEY_D))
 	{
-		this->position += glm::vec3(speed, 0.0f, 0.0f) * deltaTime;
+		this->position += glm::vec3(_speed, 0.0f, 0.0f) * deltaTime;
 	}
 
-
-	if (input()->GetKeyDown(KEY_L))
+	//Upgrades
+	if (input()->GetKeyDown(KEY_Z))
 	{
 		UpgradeSpeed();
-		std::cout << "----------------" << std::endl;
-		std::cout << "SPD is now" << std::endl;
-		std::cout << speed << std::endl;
-		std::cout << "----------------" << std::endl;
+		//std::cout << "----------------" << std::endl;
+		//std::cout << "SPD is now" << std::endl;
+		//std::cout << speed << std::endl;
+		//std::cout << "----------------" << std::endl;
 	}
-	if (input()->GetKeyDown(KEY_O))
+	if (input()->GetKeyDown(KEY_H))
 	{
 		UpgradeHitPoints();
-		std::cout << "----------------" << std::endl;
-		std::cout << "HP is now" << std::endl;
-		std::cout << health << std::endl;
-		std::cout << "----------------" << std::endl;
+		//std::cout << "----------------" << std::endl;
+		//std::cout << "HP is now" << std::endl;
+		//std::cout << health << std::endl;
+		//std::cout << "----------------" << std::endl;
 	}
-	if (input()->GetKeyDown(KEY_K))
+	if (input()->GetKeyDown(KEY_Q))
 	{
 		UpgradeBulletDamage();
-		std::cout << "----------------" << std::endl;
-		std::cout << "BDMG is now" << std::endl;
-		std::cout << bulletdamage << std::endl;
-		std::cout << "----------------" << std::endl;
+		//std::cout << "----------------" << std::endl;
+		//std::cout << "BDMG is now" << std::endl;
+		//std::cout << bulletdamage << std::endl;
+		//std::cout << "----------------" << std::endl;
 	}
-	if (input()->GetKeyDown(KEY_I))
+	if (input()->GetKeyDown(KEY_E))
 	{
 		UpgradeBulletSpeed();
-		std::cout << "----------------" << std::endl;
-		std::cout << "BSPD is now" << std::endl;
-		std::cout << bulletspeed << std::endl;
-		std::cout << "----------------" << std::endl;
+		//std::cout << "----------------" << std::endl;
+		//std::cout << "BSPD is now" << std::endl;
+		//std::cout << bulletspeed << std::endl;
+		//std::cout << "----------------" << std::endl;
 	}
-	if (input()->GetKeyDown(KEY_U))
+	if (input()->GetKeyDown(KEY_X))
 	{
 		UpgradeBalloon();
-		std::cout << "----------------" << std::endl;
-		std::cout << "Upgraded Balloon to Level" << std::endl;
-		std::cout << level << std::endl;
-		std::cout << "----------------" << std::endl;
+		//std::cout << "----------------" << std::endl;
+		//std::cout << "Upgraded Balloon to Level" << std::endl;
+		//std::cout << level << std::endl;
+		//std::cout << "----------------" << std::endl;
 	}
+
+	//Shooting bullets & LaserBeam
 	if (input()->GetKeyDown(Space))
 	{
 		beamdelay->StartOverTimer();
@@ -90,109 +96,128 @@ void Player::update(float deltaTime)
 	}
 	if (input()->GetKey(Space))
 	{
-		std::cout << beamdelay->Seconds() << std::endl;
+	/*	std::cout << beamdelay->Seconds() << std::endl;*/
+		if (beamdelay->Seconds() >= 1)
+		{
+			this->sprite = new Sprite("assets/pencils.tga");
+		}
 	}
-	if (input()->GetKeyUp(Space) && beamdelay->Seconds() >= 2)
+	if (input()->GetKeyUp(Space) && beamdelay->Seconds() >= 1)
 	{
-		std::cout << "BEAAMMM" << std::endl;
 		beamdelay->StopTimer();
 		ShootBeam();
+		this->sprite = new Sprite("assets/Balloon.tga");
 
 	}
 }
 
 void Player::ShootBeam()
 {
-	LaserBeam* laserbeam = new LaserBeam();
-	laserbeam->position = this->position;
-	bullets.push_back(laserbeam);
-	this->parent->AddChild(laserbeam);
+	Bullet* sphere = new Bullet();
+	sphere->AddSprite("assets/Sphere.tga");
+	bulletoffset = glm::vec3(20.0f, 40.0f, 0);
+	sphere->position = this->position + bulletoffset;
+	sphere->speedx = 200;
+	sphere->damage = 1;
+	sphere->health = 100;
+	bullets.push_back(sphere);
+	this->parent->AddChild(sphere);
 }
 
 
 void Player::Shoot() 
 {
-	if (level == 1)
+	if (_level == 1)
 	{
 		Bullet* bullet = new Bullet();
-		bullet->position = this->position;
-		bullet->speedx = bulletspeed;
-		bullet->damage = bulletdamage;
+		bullet->position = this->position + bulletoffset;
+		bullet->speedx = _bulletspeed;
+		bullet->damage = _bulletdamage;
 		bullets.push_back(bullet);
 		this->parent->AddChild(bullet);
 		
 	}
-	if (level == 2)
+	if (_level == 2)
 	{
 		Bullet* bullet = new Bullet();
-		bullet->position = this->position;
-		bullet->speedx = bulletspeed;
-		bullet->speedy = bulletspeed;
-		bullet->damage = bulletdamage;
+		bullet->position = this->position + bulletoffset;
+		bullet->speedx = _bulletspeed;
+		bullet->speedy = _bulletspeed / 2;
+		bullet->damage = _bulletdamage;
+		bullet->health = 2;
 		bullets.push_back(bullet);
 		this->parent->AddChild(bullet);
 
 		Bullet* bullet2 = new Bullet();
-		bullet2->position = this->position;
-		bullet2->speedx = bulletspeed;
-		bullet2->damage = bulletdamage;
+		bullet2->position = this->position + bulletoffset;
+		bullet2->speedx = _bulletspeed;
+		bullet2->damage = _bulletdamage;
+		bullet2->health = 2;
 		bullets.push_back(bullet2);
 		this->parent->AddChild(bullet2);
 	}
-	if (level == 3)
+	if (_level == 3)
 	{
 		Bullet* bullet = new Bullet();
-		bullet->position = this->position;
-		bullet->speedx = bulletspeed;
-		bullet->speedy = bulletspeed;
-		bullet->damage = bulletdamage;
+		bullet->position = this->position + bulletoffset;
+		bullet->speedx = _bulletspeed;
+		bullet->speedy = _bulletspeed /2;
+		bullet->damage = _bulletdamage;
+		bullet->health = 3;
 		bullets.push_back(bullet);
 		this->parent->AddChild(bullet);
 
 		Bullet* bullet2 = new Bullet();
-		bullet2->position = this->position;
-		bullet2->speedx = bulletspeed;
-		bullet2->damage = bulletdamage;
+		bullet2->position = this->position + bulletoffset;
+		bullet2->speedx = _bulletspeed;
+		bullet2->damage = _bulletdamage;
+		bullet2->health = 3;
 		bullets.push_back(bullet2);
 		this->parent->AddChild(bullet2);
 
 		Bullet* bullet3 = new Bullet();
-		bullet3->position = this->position;
-		bullet3->speedx = bulletspeed;
-		bullet3->speedy = -bulletspeed;
-		bullet3->damage = bulletdamage;
+		bullet3->position = this->position + bulletoffset;
+		bullet3->speedx = _bulletspeed;
+		bullet3->speedy = -_bulletspeed /2;
+		bullet3->damage = _bulletdamage;
+		bullet3->health = 3;
 		bullets.push_back(bullet3);
 		this->parent->AddChild(bullet3);
 	}
-	if (level == 4)
+	if (_level == 4)
 	{
 		Bullet* bullet = new Bullet();
-		bullet->position = this->position;
-		bullet->speedx = bulletspeed;
-		bullet->speedy = bulletspeed;
-		bullet->damage = bulletdamage;
+		bullet->position = this->position + bulletoffset;
+		bullet->speedx = _bulletspeed;
+		bullet->speedy = _bulletspeed /2;
+		bullet->damage = _bulletdamage;
+		bullet->health = 5;
 		bullets.push_back(bullet);
 		this->parent->AddChild(bullet);
 
 		Bullet* bullet2 = new Bullet();
-		bullet2->position = this->position;
-		bullet2->speedx = bulletspeed;
-		bullet2->damage = bulletdamage;
+		bullet2->position = this->position + bulletoffset;
+		bullet2->speedx = _bulletspeed;
+		bullet2->damage = _bulletdamage;
+		bullet2->health = 5;
 		bullets.push_back(bullet2);
 		this->parent->AddChild(bullet2);
 
 		Bullet* bullet3 = new Bullet();
-		bullet3->position = this->position;
-		bullet3->speedx = bulletspeed;
-		bullet3->speedy = -bulletspeed;
-		bullet3->damage = bulletdamage;
+		bullet3->position = this->position + bulletoffset;
+		bullet3->speedx = _bulletspeed;
+		bullet3->speedy = -_bulletspeed /2;
+		bullet3->damage = _bulletdamage;
+		bullet3->health = 5;
 		bullets.push_back(bullet3);
 		this->parent->AddChild(bullet3);
 
 		Bullet* bullet4 = new Bullet();
-		bullet4->position = this->position;
-		bullet4->speedx = -bulletspeed;
-		bullet4->damage = bulletdamage;
+		bulletoffset = glm::vec3(-10.0f, 40.0f, 0);
+		bullet4->position = this->position + bulletoffset;
+		bullet4->speedx = -_bulletspeed;
+		bullet4->damage = _bulletdamage;
+		bullet4->health = 5;
 		bullets.push_back(bullet4);
 		this->parent->AddChild(bullet4);
 	}
@@ -201,63 +226,62 @@ void Player::Shoot()
 }
 int Player::UpgradeBalloon() 
 {
-	if (level == 4)
+	if (_level == 4)
 	{
 		std::cout << "MAXED OUT BALLOON" << std::endl;
-		return level;
+		return _level;
 	}
-	level += 1;
-	return level;
+	_level += 1;
+	return _level;
 }
 int Player::UpgradeBulletSpeed()
 {
-	if (bulletspeed == 1000)
+	if (_bulletspeed == 1000)
 	{
 		std::cout << "MAXED OUT BSPD" << std::endl;
-		return bulletspeed;
+		return _bulletspeed;
 	}
-	bulletspeed += 50;
-	return bulletspeed;
+	_bulletspeed += 100;
+	return _bulletspeed;
 }
 int Player::UpgradeBulletDamage()
 {
-	if (bulletdamage == 6)
+	if (_bulletdamage == 6)
 	{
 		std::cout << "MAXED OUT BDMG" << std::endl;
-		return bulletdamage;
+		return _bulletdamage;
 	}
-	bulletdamage += 1;
-	return bulletdamage;
+	_bulletdamage += 1;
+	return _bulletdamage;
 }
 int Player::UpgradeFireRate()
 {
-	if (fireRate == 6)
+	if (_fireRate == 6)
 	{
 		std::cout << "MAXED OUT FR" << std::endl;
-		return fireRate;
+		return _fireRate;
 	}
-	fireRate += 1;
-	return fireRate;
+	_fireRate += 1;
+	return _fireRate;
 }
-
 
 int Player::UpgradeHitPoints()
 {
-	if (health == 6)
+	if (maxHealth == 6)
 	{
 		std::cout << "MAXED OUT HP" << std::endl;
-		return health;
+		return maxHealth;
 	}
-	health += 1;
-	return health;
+	maxHealth += 1;
+	return maxHealth;
 }
 int Player::UpgradeSpeed()
 {
-	if (speed == 500.0f)
+	if (_speed == 500.0f)
 	{
 		std::cout << "MAXED OUT SPD" << std::endl;
-		return speed;
+		return _speed;
 	}
-	speed += 50.0f;
-	return speed;
+	_speed += 50.0f;
+	return _speed;
 }
--- Bloris/Player.h
+++ Bloris/Player.h
@@ -4,7 +4,6 @@
 #include <common/entity.h>
 #include <common/timer.h>
 #include <Bloris/Bullet.h>
-#include <Bloris/LaserBeam.h>
 
 class Player : public Entity
 {
@@ -13,18 +12,32 @@ public:
 	virtual ~Player();
 
 	virtual void update(float deltaTime);
+	
+	float Speed() { return _speed; };
+	float BulletSpeed() { return _bulletspeed; };
+	float BulletDamage() { return _bulletdamage; };
+	float FireRate() { return _fireRate; };
+	int Level() { return _level; };
+
+	glm::vec3 bulletoffset;
+
+
+	bool dead;
+	int health;
+	int maxHealth;
 
 	std::vector <Bullet*> bullets;
 private:
 	Timer* shootdelay;
 	Timer* beamdelay;
-	float speed;
-	float fireRate;
-	int health;
-	int level;
+	
+	
+	float _speed;
+	float _bulletspeed;
+	float _fireRate;
+	int _bulletdamage;
+	int _level;
 
-	float bulletspeed;
-	int bulletdamage;
 
 	void Shoot();
 	void ShootBeam();
